763088e01ffe074ba9183969ebbc141f78be0761
Michael Tetzlaff <michaelt919@gmail.com>
2022-03-25 15:41:14 -0500

    Added Autumn jade sculpture that uses PTM shader (WIP)

--- Assets/Scripts/DropDown.cs
+++ Assets/Scripts/DropDown.cs
@@ -9,16 +9,12 @@ public class DropDown : MonoBehaviour
     public GameObject[] objects;
     public Text textBox;
     public Text titleBox;
-    GameObject Vase;
-    GameObject Polyxena;
-    GameObject KuanYu;
-    GameObject Ding;
-    GameObject Altarpiece;
+    GameObject currentObject;
 
     private void Start()
     {
-        KuanYu = Instantiate(objects[0], objects[0].transform.position, objects[0].transform.rotation);
-        DontDestroyOnLoad(KuanYu);
+        currentObject = Instantiate(objects[0], objects[0].transform.position, objects[0].transform.rotation);
+        DontDestroyOnLoad(currentObject);
         if (!textBox)
         {
             textBox = GameObject.Find("AnnotationTextbox").GetComponent<Text>();
@@ -43,50 +39,8 @@ public class DropDown : MonoBehaviour
         textBox.text = "";
         titleBox.text = "";
 
-        if (val == 0)
-        {
-            Destroy(Polyxena);
-            Destroy(Vase);
-            Destroy(Ding);
-            Destroy(Altarpiece);
-            KuanYu = Instantiate(objects[0], objects[0].transform.position, objects[0].transform.rotation);
-            DontDestroyOnLoad(KuanYu);
-        }
-        if (val == 1)
-        {
-            Destroy(Vase);
-            Destroy(KuanYu);
-            Destroy(Ding);
-            Destroy(Altarpiece);
-            Polyxena = Instantiate(objects[1], objects[1].transform.position, objects[1].transform.rotation);
-            DontDestroyOnLoad(Polyxena);
-        }
-        if (val == 2)
-        {
-            Destroy(KuanYu);
-            Destroy(Polyxena);
-            Destroy(Ding);
-            Destroy(Altarpiece);
-            Vase = Instantiate(objects[2], objects[2].transform.position, objects[2].transform.rotation);
-            DontDestroyOnLoad(Vase);
-        }
-        if (val == 3)
-        {
-            Destroy(Vase);
-            Destroy(Polyxena);
-            Destroy(KuanYu);
-            Destroy(Altarpiece);
-            Ding = Instantiate(objects[3], objects[3].transform.position, objects[3].transform.rotation);
-            DontDestroyOnLoad(Ding);
-        }
-        if (val == 4)
-        {
-            Destroy(Vase);
-            Destroy(Polyxena);
-            Destroy(KuanYu);
-            Destroy(Ding);
-            Altarpiece = Instantiate(objects[4], objects[4].transform.position, objects[4].transform.rotation);
-            DontDestroyOnLoad(Altarpiece);
-        }
+        Destroy(currentObject);
+        currentObject = Instantiate(objects[val], objects[val].transform.position, objects[val].transform.rotation);
+        DontDestroyOnLoad(currentObject);
     }
 }
